a9dfa82a9bc63e1b48f5ddfd08c04eb476afa627
Bakabot307 <60350866+Bakabot307@users.noreply.github.com>
2023-02-03 22:25:44 +0700

    new

--- bot.js
+++ bot.js
@@ -1,51 +1,93 @@
+const tmi = require("tmi.js");
 const SteamUser = require("steam-user");
-const client = new SteamUser();
 require("dotenv").config();
 
-const tmi = require("tmi.js");
-let stop = false;
-let fromTwitch = true;
-const clientTwitch = new tmi.Client({
-  channels: ["bakabot1235"],
-  identity: {
-    username: "bakabot1235",
-    password: "yhmglcavn5uxszj7tw8150yjzd0wfp",
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bakabobo " + port);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+const twitchClient = new tmi.Client({
+  connection: {
+    reconnect: true,
+    secure: true,
   },
+  identity: {
+    username: process.env.TWITCH_USER_NAME,
+    password: process.env.TWITCH_PASSWORD,
+  },
+  channels: ["bakabot1235"],
 });
 
-const logOnOptions = {
-  accountName: "bakabot1235",
-  password: "Thang123.",
-};
+const twitchClientMain = new tmi.Client({
+  connection: {
+    reconnect: true,
+    secure: true,
+  },
+  identity: {
+    username: process.env.TWITCH_USER_NAME_MAIN,
+    password: process.env.TWITCH_PASSWORD_MAIN,
+  },
+  channels: ["bakabot1235"],
+});
+twitchClient.connect().catch(console.log("yo"));
+twitchClientMain.connect();
+const steamClient = new SteamUser();
 
-client.logOn(logOnOptions);
+let isBotRunning = true;
 
-client.on("loggedOn", () => {
-  console.log("logged on");
-  client.setPersona(SteamUser.EPersonaState.Busy);
+steamClient.logOn({
+  accountName: process.env.STEAM_USER_NAME,
+  password: process.env.STEAM_PASSWORD,
 });
 
-client.on("friendMessage", function (steamId, message) {
-  fromTwitch = false;
-  clientTwitch.say("bakabot1235", message);
-  fromTwitch = true;
+steamClient.on("loggedOn", () => {
+  twitchClient.say(
+    "bakabot1235",
+    `/me steam connected. Bot is online HACKERMANS`
+  );
+  steamClient.setPersona(SteamUser.EPersonaState.Busy);
 });
 
-clientTwitch.connect();
+steamClient.on("friendMessage", function (steamId, message) {
+  twitchClientMain.say("bakabot1235", message);
+});
 
-clientTwitch.on("message", (channel, tags, message, self) => {
-  if (message === "!stop" && `${tags["display-name"]}` === "Bakabot1235") {
-    stop = true;
-  }
-  if ((message === "!start" && `${tags["display-name"]}`) === "Bakabot1235") {
-    stop = false;
-  }
+twitchClient.on("chat", (channel, userstate, message, self) => {
+  if (self) return;
 
-  if (fromTwitch === true && stop === false) {
-    console.log("sent");
-    client.chatMessage(
-      "76561198392179703",
-      `${tags["display-name"]}: ${message}`
+  const command = message.trim().split(" ")[0];
+
+  if (
+    command === "!start" &&
+    (userstate["mod"] || userstate["username"] === channel.slice(1))
+  ) {
+    isBotRunning = true;
+    twitchClient.say(
+      channel,
+      `/me bot started by ${userstate["username"]} Plotge `
     );
+  } else if (
+    command === "!stop" &&
+    (userstate["mod"] || userstate["username"] === channel.slice(1))
+  ) {
+    isBotRunning = false;
+    twitchClient.say(
+      channel,
+      `/me bot stopped by ${userstate["username"]} NONONONONO`
+    );
+  } else if (isBotRunning) {
+    steamClient.chatMessage(
+      "76561198392179703",
+      `${userstate["username"]}: ${message}`
+    );
+  } else {
+    return;
   }
 });

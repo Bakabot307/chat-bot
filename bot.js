677301b64ff61ee1362414510b056d16a10bbcf8
Bakabot307 <60350866+Bakabot307@users.noreply.github.com>
2023-02-12 18:11:44 +0700

    new

--- bot.js
+++ bot.js
@@ -3,6 +3,15 @@ const SteamUser = require("steam-user");
 require("dotenv").config();
 const request = require("request");
 const steamTotp = require("steam-totp");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Bakabobo " + port);
+});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
 // const twitchClient = new tmi.Client({
 //   connection: {

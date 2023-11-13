b764d7b9d9964897389fb4fd6a7e086897f7da27
Bakabot307 <60350866+Bakabot307@users.noreply.github.com>
2023-11-13 13:18:20 +0700

    test callback

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ const express = require("express");
 const catApi = require("random-cat-img");
 const app = express();
 const port = process.env.PORT || 3000;
-var bodyParser = require('body-parser');
-var received_updates = [];
+const bodyParser = require('body-parser');
+const received_updates = [];
 app.get("/", (req, res) => {
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
@@ -25,9 +25,7 @@ app.post('/instagram', function(req, res) {
   received_updates.unshift(req.body);
   res.sendStatus(200);
 });
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen();
 const receivers = [
   '76561198392179703', //IDs for example
   '22222222222222222',

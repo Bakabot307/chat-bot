b5b366041e4c202e6b12508985a874fd8c36a1bd
Bakabot307 <60350866+Bakabot307@users.noreply.github.com>
2023-11-13 12:03:38 +0700

    test callback

--- bot.js
+++ bot.js
@@ -6,26 +6,9 @@ const express = require("express");
 const catApi = require("random-cat-img");
 const app = express();
 const port = process.env.PORT || 3000;
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-const port = 3000;
-
-app.use(bodyParser.json());
-
-app.post('/webhook', (req, res) => {
-  // Handle the incoming webhook payload here
-  console.log('Webhook received:', req.body);
-  res.status(200).send('Webhook received successfully');
-});
-
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
-});
 
 app.get("/", (req, res) => {
-  res.send("Bakabobo " + port);
+  res.send("Bakabobo " + req);
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);

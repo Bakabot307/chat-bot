18fb629617ecb4dfeaf099812d03e236925bddba
Bakabot307 <60350866+Bakabot307@users.noreply.github.com>
2023-11-18 19:22:11 +0700

    twitch and steam chat

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
 const tmi = require("tmi.js");
 const SteamUser = require("steam-user");
 require("dotenv").config();
-
+const request = require("request");
+const steamTotp = require("steam-totp");
 const express = require("express");
+const catApi = require("random-cat-img");
 const app = express();
+const express = require("express");
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -40,8 +43,39 @@ twitchClientMain.connect().catch(console.log("connect to main"));
 twitchClient.connect().catch(console.log("connect to bot"));
 
 const steamClient = new SteamUser();
+const steamClientMain = new SteamUser();
 
 let isBotRunning = true;
+
+const loginDetails = {
+  accountName: process.env.STEAM_USER_NAME,
+  password: process.env.STEAM_PASSWORD,
+  twoFactorCode: steamTotp.generateAuthCode(process.env.STEAM_SHARED_SECRET),
+  rememberPassword: true,
+};
+
+steamClientMain.logOn(loginDetails);
+
+steamClientMain.on('playingState', function(blocked, playingApp) {
+  if(blocked){
+    console.log("playing: ", playingApp)
+    steamClient.setPersona(SteamUser.EPersonaState.Busy);
+  } else{
+    console.log("not playing anything")
+    steamClient.setPersona(SteamUser.EPersonaState.Busy);
+    steamClient.gamesPlayed([570])
+  }
+});
+steamClientMain.on("error", function (e) {
+  if (e.eresult === SteamUser.EResult.LoggedInElsewhere) {
+    console.log("logged somewhere else");
+    steamClient.setPersona(SteamUser.EPersonaState.Busy);
+    relogAfterDelay();
+  }
+});
+
+function relogAfterDelay() {
+}
 
 steamClient.logOn({
   accountName: process.env.STEAM_USER_NAME_2,
@@ -79,19 +113,75 @@ twitchClientMain.on("message", (channel, userstate, message, self) => {
 
     twitchClient.say(
         channel,
-        `/me bot stopped by ${userstate["username"]} NONONONONO`
+        `/me bot stopped by ${userstate["username"]} NONONONONO sending cat pics `
     );
     steamClient.chatMessage(
         "76561198392179703",
-        `/me bot stopped by ${userstate["username"]} NONONONONO`
+        `/me bot stopped by ${userstate["username"]} NONONONONO sending cat pics`
     );
   }
   if (userstate["username"] === channel.slice(1)) {
     return;
   }
   if (!isBotRunning) {
-    return;
+    function relogAfterDelay() {
+    }
+    steamClient.on("friendMessage", (steamID, message) => {
+      const result = Math.floor(Math.random() * 3) + 1;
+
+      if (result === 1) {
+        (async () => {
+          const res = await catApi();
+          steamClient.chatTyping(steamID);
+          steamClient.chatMessage(
+              steamID,
+              `${res.data.message}`,
+              SteamUser.EChatEntryType.ChatMsg
+          );
+        })();
+      } else if (result === 2) {
+        request(
+            {
+              url: "https://aws.random.cat/meow",
+              json: true,
+            },
+            (error, response, body) => {
+              if (!error && response.statusCode === 200) {
+                // Send a message to the Steam user with the URL of the random cat image
+                steamClient.chatTyping(steamID);
+                steamClient.chatMessage(
+                    steamID,
+                    `${body.file}`,
+                    SteamUser.EChatEntryType.ChatMsg
+                );
+              } else {
+                console.error(`Error getting cat image: ${error}`);
+              }
+            }
+        );
+      } else {
+        request(
+            {
+              url: "https://api.thecatapi.com/v1/images/search",
+              json: true,
+            },
+            (error, response, body) => {
+              if (!error && response.statusCode === 200) {
+                // Send a message to the Steam user with the URL of the random cat image
+                steamClient.chatTyping(steamID);
+                steamClient.chatMessage(
+                    steamID,
+                    `${body[0].url}`,
+                    SteamUser.EChatEntryType.ChatMsg
+                );
+              } else {
+                console.error(`Error getting cat image: ${error}`);
+              }
+            }
+        );
+      }
   }
+  });
   steamClient.chatMessage(
       "76561198392179703",
       `${userstate["username"]}: ${message}`
